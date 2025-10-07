98bd29f6e8a579ea3b5275b9c52725df74deb701
Nafis Mohammed Jami <nafismohammedjami@gmail.com>
2025-10-08 00:37:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
 hamburger.addEventListener('click', () => {
-  navMenu.querySelector('ul').classList.toggle('show');
+ navMenu.querySelector('ul').classList.toggle('show');
 });
