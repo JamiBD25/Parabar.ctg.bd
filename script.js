eb7d37197366490360a2f154b141665f11545009
Nafis Mohammed Jami <nafismohammedjami@gmail.com>
2026-02-23 02:41:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamburger = document.getElementById('hamburger');
-const navMenu = document.querySelector('#nav-menu ul');
-
+const navMenu = document.querySelector
+('nav-menu');
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('show');
   hamburger.classList.toggle('active'); // হ্যামবার্গার X এনিমেশন
